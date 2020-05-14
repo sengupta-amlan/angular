d76426d11a0a04ff052ba3c6c5463ce0fc459aca
sengupta-amlan <64716923+sengupta-amlan@users.noreply.github.com>
2020-05-14 10:04:10 +0530

    navigate on header component

--- projectAngular/src/app/header/header.component.ts
+++ projectAngular/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Output} from '@angular/core';
+
 
 @Component(
   {
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
   }
 )
 export class HeaderComponent{
-
+  @Output() featureSelected = new EventEmitter<string>();
+  onSelect(feature:string){
+    this.featureSelected.emit(feature);
+  }
 }
